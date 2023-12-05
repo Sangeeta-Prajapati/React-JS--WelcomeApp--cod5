bfd5945c29e8eafde210e574f8e554bebeb013ec
Sangeeta <sanjuother107@gmail.com>
2023-12-05 15:55:09 +0000

    welcome app soln

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -6,41 +6,31 @@ import './index.css'
 class Welcome extends Component {
   state = {isSubscribe: true}
 
-  oSubscribe = () => {
-    this.setState(prevState => ({
-      isSubscribe: !prevState.isSubscribe,
-    }))
-  }
-
-  getButtonText = () => {
+  OnSubscribe = () => {
     const {isSubscribe} = this.state
-
     if (isSubscribe === true) {
-      return (
-        <button className="button" type="button" onClick={this.OnSubscribe}>
-          {isSubscribe}
-        </button>
-      )
+      this.setState({isSubscribe: false})
+    } else {
+      this.setState({isSubscribe: true})
     }
-    return (
-      <button className="button" type="button" onClick={this.OnSubscribe}>
-        {isSubscribe}
-      </button>
-    )
   }
 
   render() {
-    const buttonText = this.getButtonText()
+    const {isSubscribe} = this.state
 
     return (
       <div className="bg-container">
         <h1 className="heading">Welcome</h1>
         <p className="des">Thank you!Happy Learning</p>
-        {/*  <button className="button" type="button" onClick={this.OnSubscribe}>
-          {buttonText}
-        </button> */}
-
-        {this.getButtonText()}
+        {isSubscribe ? (
+          <button className="button" type="button" onClick={this.OnSubscribe}>
+            Subscribe
+          </button>
+        ) : (
+          <button className="button" type="button" onClick={this.OnSubscribe}>
+            Subscribed
+          </button>
+        )}
       </div>
     )
   }
